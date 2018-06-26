40bd691ce7a3300515f0f1694a086cf0fd83430d
prmoreira23 <prm.gredes@gmail.com>
2018-06-26 11:25:51 -0400

    Add each, map, reduce, find.

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, iteratee) {
+      let values = Object.values(collection);
+      for(let i = 0; i < values.length; i++){
+        iteratee(values[i]);
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, iteratee) {
+      let values = Object.values(collection);
+      for(let i = 0; i < values.length; i++){
+        values[i] = iteratee(values[i]);
+      }
+      return values;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, iteratee, acc = 0) {
+      for(let i = 0; i < collection.length; i++){
+        acc = iteratee(acc, collection[i], collection);
+      }
+      return acc;
     },
-    
+
+    find: function(collection, predicate) {
+      for(let i = 0; i < collection.length; i++){
+        if(predicate(collection[i])){
+          return collection[i];
+        }
+      }
+    },
+
     functions: function() {
 
     },
